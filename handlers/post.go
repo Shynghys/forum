dc939a30532d50b20b73fb315375d8b20206927e
shynghys <shynghys@mail.ru>
2020-11-03 14:15:22 +0600

    post

--- handlers/post.go
+++ handlers/post.go
@@ -1,15 +1,17 @@
 package handlers
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 // PostsHandler gets post by id
 func PostsHandler(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+	tmpl := template.Must(template.ParseFiles("templates/sign-up.html"))
 
-	fmt.Fprintf(w, "You've requested the user: id = %s \n")
+	tmpl.Execute(w, nil)
+	// fmt.Fprintf(w, "You've requested the user: id = %s \n")
 }
 
 // PostHandler gets post by id
@@ -25,27 +27,35 @@ func PostsHandler(w http.ResponseWriter, r *http.Request) {
 func CreatePost(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 	// db.AddPost()
+	tmpl := template.Must(template.ParseFiles("templates/sign-up.html"))
 
-	fmt.Fprintf(w, "You've requested the user: id = %s \n")
+	tmpl.Execute(w, nil)
+
 }
 
 // ReadPost gets post by id
 func ReadPost(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 
-	fmt.Fprintf(w, "You've requested the user: id = %s \n")
+	tmpl := template.Must(template.ParseFiles("templates/sign-up.html"))
+
+	tmpl.Execute(w, nil)
 }
 
 // UpdatePost gets post by id
 func UpdatePost(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 
-	fmt.Fprintf(w, "You've requested the user: id = %s \n")
+	tmpl := template.Must(template.ParseFiles("templates/sign-up.html"))
+
+	tmpl.Execute(w, nil)
 }
 
 // DeletePost gets post by id
 func DeletePost(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 
-	fmt.Fprintf(w, "You've requested the user: id = %s \n")
+	tmpl := template.Must(template.ParseFiles("templates/sign-up.html"))
+
+	tmpl.Execute(w, nil)
 }
