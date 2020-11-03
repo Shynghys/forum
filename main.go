cccbc5c87f110e72dbd114eabd73053b12500ec7
d0ssan <yerdos29@gmail.com>
2020-11-03 13:25:56 +0600

    added tool to parse all templates and router

--- main.go
+++ main.go
@@ -13,19 +13,14 @@ import (
 func main() {
 	//Create db
 	newDb := db.CreateDatabase()
-	// fmt.Println(newDb)
+	fmt.Println(newDb)
 
 	// Starting web server
 
 	// http.HandleFunc("/", MakeRouter)
-	http.HandleFunc("/", h.Handler)
-	http.HandleFunc("/sign_in", h.SignInHandler)
-	http.HandleFunc("/sign_up", h.SignUpHandler)
-
-	http.HandleFunc("/user/{id}", h.UserHandler)
-
-	http.HandleFunc("/posts", h.PostsHandler)
-	http.HandleFunc("/posts/{title}", h.CreatePost)
+	h.LoadTemplates("templates/*.html")
+	r := h.NewRouter()
+	http.Handle("/", r)
 	fmt.Println("serving..............")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
