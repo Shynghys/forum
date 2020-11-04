50b990c5a9877815783a25943c87201e851df1ae
buterbrot359 <batowka359@gmail.com>
2020-11-04 17:04:09 +0600

    CRUDdatabaseFunctions

---
+++ database/post.go
@@ -0,0 +1,48 @@
+package data
+
+import (
+	"database/sql"
+
+	"../vars"
+	uuid "github.com/satori/go.uuid"
+)
+
+func AddPost(db *sql.DB, post vars.Post) {
+	tx, _ := db.Begin()
+	id := CreatedUID()
+	stmt, _ := tx.Prepare("INSERT INTO posts (id, authorID, title, created, category, likes) VALUES (?,?,?,?,?,?)")
+	// stmt.Exec(id, username, email, password, created)
+	_, err := stmt.Exec(id, post.AuthorID, post.Title, post.Created, post.Category, post.Likes)
+	CheckErr(err)
+	tx.Commit()
+}
+func GetPost(db *sql.DB, id2 uuid.UUID) vars.Post {
+	rows, err := db.Query("SELECT * FROM posts")
+	CheckErr(err)
+	for rows.Next() {
+		var tempPost vars.Post
+		err =
+			rows.Scan(&tempPost.ID, &tempPost.AuthorID, &tempPost.Title, &tempPost.Created, &tempPost.Category, &tempPost.Likes /*, &tempPost.posts, &tempPost.comments*/)
+		CheckErr(err)
+		if tempPost.ID == id2 {
+			return tempPost
+		}
+	}
+	return vars.Post{}
+}
+func UpdatePost(db *sql.DB, toChange vars.Post) {
+
+	tx, _ := db.Begin()
+	stmt, _ := tx.Prepare("UPDATE posts SET title=?, category=?, likes=? WHERE id=?")
+	_, err := stmt.Exec(toChange.Title, toChange.Category, toChange.Likes, toChange.ID)
+	CheckErr(err)
+	tx.Commit()
+}
+func DeletePost(db *sql.DB, id uuid.UUID) {
+
+	tx, _ := db.Begin()
+	stmt, _ := tx.Prepare("DELETE FROM posts WHERE id=?")
+	_, err := stmt.Exec(id)
+	CheckErr(err)
+	tx.Commit()
+}
--- database/test/main.go
+++ database/test/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	d ".."
+	v "../../vars"
 )
 
 func main() {
 	// fmt.Println("1")
 	newDB := d.CreateDatabase()
-	d.AddPost(newDB, d.CreatedUID(), d.CreatedUID(), "x", "x", "x", 3)
-	d.AddUser(newDB, "buterbrot", "bat@mail.ru", "abc", "123456")
+	var a v.User
+	a.Username = "ButerBrot"
+	a.Email = "batowka359@gmail.com"
+	a.Password = "abc"
+	a.Created = "21-03-1997"
+	d.AddUser(newDB, a)
+	// d.AddUser(newDB, "ray-bang8", "kanatkyzy@gmail.ru", "abc", "01-02-1998")
+	// d.AddUser(newDB, "Dossan", "Yerdos@gmail.ru", "abc", "18-04-1997")
+	// d.AddPost(newDB, d.CreatedUID(), d.AllUsers[0].ID, "x", "x", "x", 3)
+	// d.UpdateUser(newDB, d.AllUsers[0].ID, "x", "x", "as")
+	// d.DeleteUser(newDB, d.AllUsers[1].ID)
+	// fmt.Println(d.GetUser(newDB, d.AllUsers[0].ID))
+
+	// fmt.Println(d.AllUsers)
+	// d.AddComment(newDB, d.CreatedUID(), d.CreatedUID(), d.CreatedUID(), "xx", "a", 3)
 
 }
