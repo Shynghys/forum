c15c3fc15da488b4e5435ea807784da6276c9171
Kabzhan Shynghys <shynghys@mail.ru>
2020-11-06 19:03:16 +0600

    error handler/template|| add little things to handlers

--- handlers/comment.go
+++ handlers/comment.go
@@ -10,7 +10,10 @@ import (
 
 // CommentsHandler gets Comments
 func CommentsHandler(w http.ResponseWriter, r *http.Request) {
-
+	if !(r.URL.Path == "/comments/") {
+		ErrorHandler(w, r, http.StatusNotFound)
+		return
+	}
 	if r.Method == "GET" {
 		tmpl := template.Must(template.ParseFiles("templates/sign-up.html"))
 
@@ -52,7 +55,11 @@ func CommentsHandler(w http.ResponseWriter, r *http.Request) {
 // CreateComment gets post by id
 func CreateComment(w http.ResponseWriter, r *http.Request) {
 
-	// db.AddPost()
+	if !(r.URL.Path == "/comments/{id}/create") {
+		ErrorHandler(w, r, http.StatusNotFound)
+		return
+	}
+
 	if r.Method == "GET" {
 		tmpl := template.Must(template.ParseFiles("templates/sign-up.html"))
 
@@ -66,22 +73,14 @@ func CreateComment(w http.ResponseWriter, r *http.Request) {
 			tmpl.Execute(w, nil)
 			return
 		}
-		details := vars.User{
-			Email:    r.FormValue("email"),
-			Username: r.FormValue("username"),
-			Password: r.FormValue("password"),
+		details := vars.Comment{
+			Text: r.FormValue("text"),
 		}
-		// db.AddUser(details)
-		// password := r.FormValue("confirmation-password")
-		// if details.Password != password {
-		// 	fmt.Println("did not match")
-		// }
+		// db.AddComment(details)
 
 		// do something with details
 		_ = details
-		fmt.Println(details.Email)
-		// do something with details
-		_ = details
+
 		tmpl.Execute(w, struct{ Success bool }{true})
 		// tmpl.Execute(w, struct{ Success bool }{true})
 		http.Redirect(w, r, "/", http.StatusSeeOther)
@@ -93,7 +92,10 @@ func CreateComment(w http.ResponseWriter, r *http.Request) {
 
 // ReadComment gets Comment by id
 func ReadComment(w http.ResponseWriter, r *http.Request) {
-
+	if !(r.URL.Path == "/comments/{id}") {
+		ErrorHandler(w, r, http.StatusNotFound)
+		return
+	}
 	if r.Method == "GET" {
 		tmpl := template.Must(template.ParseFiles("templates/sign-up.html"))
 
@@ -134,7 +136,10 @@ func ReadComment(w http.ResponseWriter, r *http.Request) {
 
 // UpdateComment gets Comment by id
 func UpdateComment(w http.ResponseWriter, r *http.Request) {
-
+	if !(r.URL.Path == "/comments/{id}/update") {
+		ErrorHandler(w, r, http.StatusNotFound)
+		return
+	}
 	if r.Method == "GET" {
 		tmpl := template.Must(template.ParseFiles("templates/sign-up.html"))
 
@@ -153,14 +158,8 @@ func UpdateComment(w http.ResponseWriter, r *http.Request) {
 			Username: r.FormValue("username"),
 			Password: r.FormValue("password"),
 		}
-		// db.AddUser(details)
-		// password := r.FormValue("confirmation-password")
-		// if details.Password != password {
-		// 	fmt.Println("did not match")
-		// }
+		// db.UpdateComment(details)
 
-		// do something with details
-		_ = details
 		fmt.Println(details.Email)
 		// do something with details
 		_ = details
@@ -175,7 +174,10 @@ func UpdateComment(w http.ResponseWriter, r *http.Request) {
 
 // DeleteComment gets Comment by id
 func DeleteComment(w http.ResponseWriter, r *http.Request) {
-
+	if !(r.URL.Path == "/comments/{id}/delete") {
+		ErrorHandler(w, r, http.StatusNotFound)
+		return
+	}
 	if r.Method == "GET" {
 		tmpl := template.Must(template.ParseFiles("templates/sign-up.html"))
 
@@ -195,16 +197,9 @@ func DeleteComment(w http.ResponseWriter, r *http.Request) {
 			Password: r.FormValue("password"),
 		}
 		// db.AddUser(details)
-		// password := r.FormValue("confirmation-password")
-		// if details.Password != password {
-		// 	fmt.Println("did not match")
-		// }
 
-		// do something with details
 		_ = details
-		fmt.Println(details.Email)
-		// do something with details
-		_ = details
+
 		tmpl.Execute(w, struct{ Success bool }{true})
 		// tmpl.Execute(w, struct{ Success bool }{true})
 		http.Redirect(w, r, "/", http.StatusSeeOther)
