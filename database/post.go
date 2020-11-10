b131a1db586c4d97a9169a83598283eca2fa7bf9
Kabzhan Shynghys <shynghys@mail.ru>
2020-11-10 16:42:09 +0600

    handler creates post

--- database/post.go
+++ database/post.go
@@ -2,18 +2,39 @@ package data
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 
 	"../vars"
 	uuid "github.com/satori/go.uuid"
 )
 
-func CreatePost(db *sql.DB, post vars.Post) {
+func CreatePost(post *vars.Post) {
+
+	db, er := sql.Open("sqlite3", "./mainDB.db")
+	CheckErr(er)
+	defer db.Close()
 	tx, _ := db.Begin()
-	id := CreatedUID()
-	stmt, _ := tx.Prepare("INSERT INTO posts (id, authorID, title, created, category, likes) VALUES (?,?,?,?,?,?)")
+
+	// id := CreatedUID()
+	fmt.Println(post.AuthorID)
+	fmt.Println(post.Title)
+	fmt.Println(post.Text)
+	fmt.Println(post.Created)
+	fmt.Println(post.Likes)
+	fmt.Println("post")
+
+	result, err := db.Exec("INSERT INTO posts (id, authorID, title, text, created, category, likes) VALUES (?,?,?,?,?,?,?)", post.ID, post.AuthorID, post.Title, post.Text, post.Created, post.Category, post.Likes)
+	// stmt, err := tx.Prepare("INSERT INTO posts (id, authorID, title, text, created, category, likes) VALUES (?,?,?,?,?,?,?)")
 	// stmt.Exec(id, username, email, password, created)
-	_, err := stmt.Exec(id, post.AuthorID, post.Title, post.Created, post.Category, post.Likes)
-	CheckErr(err)
+	// _, err := stmt.Exec(post.ID, post.AuthorID, post.Title, post.Text, post.Created, post.Category, post.Likes)
+	fmt.Println(result)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// CheckErr(err)
+	fmt.Println("Post created!!!!1")
 	tx.Commit()
 }
 func ReadPost(db *sql.DB, id2 uuid.UUID) vars.Post {
--- handlers/post.go
+++ handlers/post.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 
+	db "../database/"
 	"../vars"
 )
 
@@ -42,32 +44,43 @@ func PostsHandler(w http.ResponseWriter, r *http.Request) {
 // CreatePost gets post by id
 func CreatePost(w http.ResponseWriter, r *http.Request) {
 
-	if !(r.URL.Path == "/posts/{id}/create") {
-		ErrorHandler(w, r, http.StatusNotFound)
-		return
-	}
+	// if !(r.URL.Path == "/posts/create") {
+	// 	ErrorHandler(w, r, http.StatusNotFound)
+	// 	return
+	// }
 	if r.Method == "GET" {
-		tmpl := template.Must(template.ParseFiles("templates/create-post.html"))
+		tmpl := template.Must(template.ParseFiles("templates/createpost.html"))
 
 		tmpl.Execute(w, nil)
 
 	}
 
 	if r.Method == "POST" {
-		tmpl := template.Must(template.ParseFiles("templates/sign-up.html"))
-		if r.Method != http.MethodPost {
-			tmpl.Execute(w, nil)
+		fmt.Println("wwwwwwwwwoooooo")
+		// tmpl := template.Must(template.ParseFiles("templates/createpost.html"))
+		// if r.Method != http.MethodPost {
+		// 	tmpl.Execute(w, nil)
+		// 	return
+		// }
+		// Call ParseForm() to parse the raw query and update r.PostForm and r.Form.
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprintf(w, "ParseForm() err: %v", err)
 			return
 		}
-		details := vars.Post{
+		fmt.Println("000000000000")
+		details := &vars.Post{
+
 			Title:    r.FormValue("title"),
 			Text:     r.FormValue("text"),
 			Category: r.FormValue("category"),
+			Created:  "0",
+			Likes:    12,
 		}
-		// db.AddPost(details)
-
-		_ = details
-		tmpl.Execute(w, struct{ Success bool }{true})
+		fmt.Println(details)
+		fmt.Println("111111111111111")
+		db.CreatePost(details)
+		fmt.Println("2222222222222")
+		// tmpl.Execute(w, struct{ Success bool }{true})
 		// tmpl.Execute(w, struct{ Success bool }{true})
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 
